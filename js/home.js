0c9e0f3a6115bd533869539bd60a3aed7a3ca34d
SuayMack <pry_chu@hotmail.com>
2023-07-04 23:18:11 -0300

    alterações no temporizador

--- js/home.js
+++ js/home.js
@@ -1,31 +1,36 @@
 /*  Código disponível em 
 https://www.w3schools.com/howto/howto_js_countdown.asp
+
+** com pequenas alterações
+
 */
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+let temporizador = new Date("Sep 5, 2023 15:37:25").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
+let x = setInterval(function() {
 
   // Get today's date and time
-  var now = new Date().getTime();
+  let agora = new Date().getTime();
     
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+  // Find the tempo between now and the count down date
+  let tempo = temporizador - agora;
     
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  let dias = Math.floor(tempo / (1000 * 60 * 60 * 24));
+  let horas = Math.floor((tempo % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutos = Math.floor((tempo % (1000 * 60 * 60)) / (1000 * 60));
+  let segundos = Math.floor((tempo % (1000 * 60)) / 1000);
     
   // Output the result in an element with id="temporizador"
-  document.getElementById("temporizador").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-    
+  document.getElementById("dias").innerHTML = dias
+  document.getElementById("horas").innerHTML = horas
+  document.getElementById("minutos").innerHTML = minutos
+  document.getElementById("segundos").innerHTML = segundos
+
   // If the count down is over, write some text 
-  if (distance < 0) {
+  if (tempo < 0) {
     clearInterval(x);
     document.getElementById("temporizador").innerHTML = "EXPIRED";
   }
